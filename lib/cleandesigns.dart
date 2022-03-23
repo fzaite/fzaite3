517bece7774b0b2ea8ca0c90ca97d7a50a41abd7
fzaite <fzaite@itgsolutions.com>
2022-03-23 16:02:12 +0300

    NEW

--- lib/cleandesigns.dart
+++ lib/cleandesigns.dart
@@ -26,32 +26,45 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        title: const Text(
-          'EXPLORE',
-          style: TextStyle(
-              fontFamily: 'Montserrat', fontSize: 20.0, color: Colors.black),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.home),
-          color: Colors.black,
-          iconSize: 27.0,
-          onPressed: () {
-            Navigator.of(context).pushNamed('/home');
-          },
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: const Text(
+            'EXPLORE',
+            style: TextStyle(
+                fontFamily: 'Montserrat', fontSize: 20.0, color: Colors.black),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(Icons.home),
             color: Colors.black,
             iconSize: 27.0,
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed('/home');
+            },
           ),
-        ],
-      ),
-    );
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.search),
+              color: Colors.black,
+              iconSize: 27.0,
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.all(10.0),
+          children: <Widget>[
+            Container(
+              height: 70.0,
+              width: 70.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35.0),
+                color: Colors.orange,
+              ),
+            )
+          ],
+        ));
   }
 }
